64d5698ebef3faced73a2d2dc5a72ff7135744c1
Ramon Raupp <ramonrooliveira@gmail.com>
2021-04-21 14:01:09 -0300

    :sparkles: update email service

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,19 +6,53 @@ import emailjs from 'emailjs-com'
 const ContactForm = ({}) => {
   
   const [isSent, setIsSent] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [selectedContact, setSelectedContact] = useState('contato')
+
+  const contactInfo = {
+    contato: {
+      serviceId: 'service_nlxssga',
+      templateId: 'template_cnykivm',
+      userId: 'user_OAavZ5dDxwZK8IIPGspfh'
+    },
+    agenda: {
+      serviceId: 'service_e0fob3s',
+      templateId: 'template_ry25oyz',
+      userId: 'user_R7nz3sZNaogQfrmFY2U6I'
+    },
+    diretoria: {
+      serviceId: 'service_jebf79j',
+      templateId: 'template_onypiad',
+      userId: 'user_2VyA5Sypn4p1aVW6xyhb7'
+    },
+    pub: {
+      serviceId: 'service_lhiwf7b',
+      templateId: 'template_sv0b22k',
+      userId: 'user_8TLa866ahK9uoWqghpBZz'
+    }
+  }
 
   const handleSubmit = (e) => {
-    console.log("submiiiiit")
-    
+    setIsLoading(true)
     e.preventDefault()
-
-    emailjs.sendForm("service_13n34df","template_8re6gma", e.target, "user_GtSDoCP2kLWvfunbPwpaB")
-      .then((result) => {
+    
+    emailjs.sendForm(
+      `${contactInfo[`${selectedContact}`].serviceId}`,
+      contactInfo[`${selectedContact}`].templateId, 
+      e.target, 
+      contactInfo[`${selectedContact}`].userId
+    ).then((result) => {
         console.log({result})
         setIsSent(true)
+        setIsLoading(false)
       }, (error) => {
         console.log({error})
+        setIsLoading(false)
       })
+  }
+
+  const handleContactChange = (e) => {
+    setSelectedContact(e.target.value)
   }
 
 
@@ -26,21 +60,39 @@ const ContactForm = ({}) => {
     <div className={styles.container}>
       <span className={styles.title}>FALE CONOSCO</span>
       <form onSubmit={handleSubmit}>
+        {!isSent && <>
         <input type="hidden" name="to_name" value="Casa de Espetáculos"></input>
         <input placeholder="Nome" name="user_name" type="text" className={styles.formInput}></input>
         <input placeholder="Email" name="from_name" type="text" className={styles.formInput}></input>
-        <select placeholder="Motivo" type="text" className={styles.formInput}>
-          <option>Contato</option>
-          <option>Agendamentos</option>
-          <option>Pub</option>
-          <option>Diretoria</option>
+        <select 
+          placeholder="Motivo" 
+          type="text" 
+          className={styles.formInput}
+          onChange={handleContactChange}
+        >
+          <option value="contato">Contato</option>
+          <option value="agenda">Agendamentos</option>
+          <option value="pub">Pub</option>
+          <option value="diretoria">Diretoria</option>
         </select>
         <textarea placeholder="Sua mensagem" name="message" className={styles.formInput}></textarea>
+        </>
+        }
 
-        <button type="submit">ENVIAR</button>
-      {/* {isSent &&
+      {!isLoading && !isSent &&
+        <button type="submit">
+          ENVIAR
+        </button>
+      }
+      {!isLoading && isSent &&
+        <>
+        <span>Mensagem enviada :)</span>
         <button onClick={() => setIsSent(false)}>Enviar outra mensagem</button>
-      } */}
+        </>
+      }
+      {isLoading &&
+        <span>is loading</span>
+      }
       </form>
       {/* <TextBlock text="instagram" classes="black"/> */}
       <div className={styles.socialLinks}>
